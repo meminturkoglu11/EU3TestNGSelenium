50259df619c2b98f60726c5f88c318491a2f3bdd
meminturkoglu11 <70228536+meminturkoglu11@users.noreply.github.com>
2020-10-07 10:11:07 +0300

    Deneme1

---
+++ src/test/java/com/cybertek/tests/Homework/gitDeneme.java
@@ -0,0 +1,13 @@
+package com.cybertek.tests.Homework;
+
+import org.testng.annotations.Test;
+
+public class gitDeneme {
+    public static void main(String[] args) {
+        System.out.println("Mehmet");
+        System.out.println("Deneme");
+        ///"mature"
+       // "nature"
+    }
+
+}
